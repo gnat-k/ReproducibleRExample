0eb1cd5504279844f9afc309f502927671624d45
N Kennedy <ka491943@ens.uqam.ca>
2023-05-18 10:49:23 -0400

    Change Site, Season and Habitat to Factor data

--- R/01_tidy_data.R
+++ R/01_tidy_data.R
@@ -3,7 +3,12 @@ library(usethis)
 
 
 usethis::use_description() #create a DESCRIPTION file
-
 usethis::use_mit_license() #create a LICENSE file
 
 bat_data <- read_csv("data/bat_dat.csv")
+
+bat_data <-
+  mutate(bat_data, 
+       Site = as.factor(Site), 
+       Habitat = as.factor(Habitat),
+       Season = as.factor(Season))
