f5e31ddd229ea215ff004defa6579d6a221b979f
N Kennedy <ka491943@ens.uqam.ca>
2023-05-18 11:01:02 -0400

    Remove Avoca site from dataset

--- R/01_tidy_data.R
+++ R/01_tidy_data.R
@@ -9,6 +9,7 @@ bat_data <- read_csv("data/bat_dat.csv")
 
 bat_data <-
   mutate(bat_data, 
-       Site = as.factor(Site), 
-       Habitat = as.factor(Habitat),
-       Season = as.factor(Season))
+      Site = as.factor(Site), 
+      Habitat = as.factor(Habitat), 
+      Season = as.factor(Season)) %>% 
+  filter(Site != "Avoca")
